5106779bd4cdb56906d9aea7adc6ea37d70fbd1a
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2020-11-01 16:51:35 +0100

    misstyping mistake

--- src/main/java/classstructuremethods/Client.java
+++ src/main/java/classstructuremethods/Client.java
@@ -29,6 +29,6 @@ public class Client {
         this.address = address;
     }
     public void migrate(String newAddress){
-        this.name=newAddress;
+        this.address=newAddress;
     }
 }
--- src/main/java/classstructuremethods/ClientMain.java
+++ src/main/java/classstructuremethods/ClientMain.java
@@ -2,18 +2,18 @@ package classstructuremethods;
 
 public class ClientMain {
     public static void main(String[] args) {
-        Client client=new Client();
+        Client client = new Client();
 
         client.setName("Szabadkai János");
         client.setAddress("6000, Kecskemét Magyar utca 27.");
         client.setYear(1969);
 
-        System.out.println("Name: "+client.getName());
-        System.out.println("Address: "+client.getAddress());
-        System.out.println("Birth of year: "+client.getYear());
+        System.out.println("Name: " + client.getName());
+        System.out.println("Address: " + client.getAddress());
+        System.out.println("Birth of year: " + client.getYear());
 
         client.migrate("6000, Kecskemét Damjanich utca 1.");
         System.out.println("The change of address was successful!");
-        System.out.println("The new address: "+client.getAddress());
+        System.out.println("The new address: " + client.getAddress());
     }
 }

9e78b36e9a1f07332b88f946acee23549a8de339
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2020-11-05 11:04:15 +0100

    Upper class was added

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,11 @@
+package javaapi;
+
+public class Upper {
+    public static void main(String[] args) {
+
+        String string = "Hello World!";
+
+        System.out.println(string.toUpperCase());
+    }
+
+}

c257009c65ec62f98d550a7451045d3196947ee1
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2020-11-05 15:45:00 +0100

    InputName class is added

---
+++ src/main/java/week02/InputName.java
@@ -0,0 +1,20 @@
+package week02;
+
+import java.util.Scanner;
+
+public class InputName {
+    public static void main(String[] args) {
+
+        String[] nevsor = new String[5];
+        Scanner scanner=new Scanner(System.in);
+
+        for (int i = 0; i < nevsor.length; i++) {
+            System.out.println("Add meg az " + i + " elemet : ");
+            nevsor[i]=scanner.nextLine();
+        }
+        for (int i = 1; i <= nevsor.length; i++) {
+            System.out.println(i+" elem: "+nevsor[i]);
+        }
+
+    }
+}

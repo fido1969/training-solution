15ab2b0622091d795bf403b1ea2a13a93b90d2e7
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2020-11-18 00:31:38 +0100

    It was modified.

--- src/main/java/debug/employee/CompanyMain.java
+++ src/main/java/debug/employee/CompanyMain.java
@@ -6,25 +6,25 @@ public class CompanyMain {
 
     public static void main(String[] args) {
 
-        Employee employee1 = new Employee("Szabadkai János", 1969);
+        /*Employee employee1 = new Employee("Szabadkai János", 1969);
         Employee employee2 = new Employee("Nagy Mariann", 1999);
         Employee employee3 = new Employee("Kiss Béla", 1956);
-        Employee employee4 = new Employee("Andreas Wollrath", 1982);
+        Employee employee4 = new Employee("Andreas Wollrath", 1982);*/
 
         Company company = new Company(new ArrayList<>());
 
-        company.addEmployee(employee1);
-        company.addEmployee(employee2);
-        company.addEmployee(employee3);
-        company.addEmployee(employee4);
+        company.addEmployee(new Employee("Szabadkai János", 1969));
+        company.addEmployee(new Employee("Nagy Mariann", 1999));
+        company.addEmployee(new Employee("Kiss Béla", 1956));
+        company.addEmployee(new Employee("Andreas Wollrath", 1982));
 
         /*Employee Kiss = company.findEmployeeByName("Kiss Béla");
         System.out.println(Kiss.getName()+" "+Kiss.getYearOfBirth());*/
 
-        System.out.println(company.findEmployeeByName("Kiss Béla").getName() + "" + company.findEmployeeByName("Kiss Béla").getYearOfBirth());
+        System.out.println(company.findEmployeeByName("Kiss Béla").getName() + "  " + company.findEmployeeByName("Kiss Béla").getYearOfBirth());
 
         System.out.println(company.listEmployeeNames());
 
-    }
+     }
 
 }

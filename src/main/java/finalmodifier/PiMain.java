65543d6f58bd0e30dffdfb870bdd6f595ead0a51
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2020-11-19 00:14:23 +0100

    PiMain class was added

---
+++ src/main/java/finalmodifier/PiMain.java
@@ -0,0 +1,20 @@
+package finalmodifier;
+
+import static finalmodifier.CircleCalculator.PI;
+
+public class PiMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("The value of PI: " + CircleCalculator.PI);
+        System.out.println();
+        System.out.println("The perimeter of the circle: " + new CircleCalculator().calculatePerimeter(2));
+        System.out.println();
+        System.out.println("The area of the circle: " + new CircleCalculator().calculateArea(2));
+        System.out.println();
+        System.out.println("The surfaceArea of cylinder: " + new CylinderCalculator().calculateSurfaceArea(2, 2));
+        System.out.println();
+        System.out.println("The volume of the cylinder: " + new CylinderCalculator().calculateVolume(2, 2));
+
+    }
+}

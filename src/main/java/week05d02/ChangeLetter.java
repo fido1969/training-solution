01884f0f0d92d10272ef87e36add89cd28e731a8
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2020-11-24 21:10:42 +0100

    ChangeLetter class was modified

--- src/main/java/week05d02/ChangeLetter.java
+++ src/main/java/week05d02/ChangeLetter.java
@@ -5,16 +5,28 @@ public class ChangeLetter {
     private static final String VOWELS = "aeiou";
 
     public String changeVowels(String text) {
-
         //if (text == null) throw new IllegalArgumentException("text is null");
         //if (text.isBlank()) throw new IllegalArgumentException("text is empty");
-
         String replacedText = "";
-        for (int i=0; i<text.length();i++){
-            if (VOWELS.indexOf(text.toLowerCase().charAt(i))>-1){
-                replacedText+="*";
-            }else {
-                replacedText+=text.charAt(i);
+
+        for (int i = 0; i < text.length(); i++) {
+            if (VOWELS.indexOf(text.toLowerCase().charAt(i)) > -1) {
+                replacedText += "*";
+            } else {
+                replacedText += text.charAt(i);
+            }
+        }
+        return replacedText;
+    }
+
+    public String changeVowels1(String text) {
+
+        String replacedText = text;
+        for (int i = 0; i < text.length(); i++) {
+            for (char c : VOWELS.toCharArray()) {
+                if (c == text.toLowerCase().charAt(i)) {
+                    replacedText = replacedText.replace(text.charAt(i), '*');
+                }
             }
         }
         return replacedText;
@@ -23,7 +35,7 @@ public class ChangeLetter {
     public static void main(String[] args) {
 
         ChangeLetter changeLetter = new ChangeLetter();
-        System.out.println(changeLetter.changeVowels("aeioumnbvCxyíűáéLKjhgfdsúőpztRWqóüö"));
-
+        System.out.println(changeLetter.changeVowels("AeIoUmnbvCxyíűáéLKjhgfdsúőpztRWqóüö"));
+        System.out.println(changeLetter.changeVowels("AeIoUmnbvCxyíűáéLKjhgfdsúőpztRWqóüö"));
     }
 }

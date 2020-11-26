96916e580bf5248ab900e2197621d446fa754a50
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2020-11-26 21:38:35 +0100

    Store class was added again

--- src/main/java/week05d04/Store.java
+++ src/main/java/week05d04/Store.java
@@ -1,6 +1,5 @@
 package week05d04;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,28 +13,38 @@ public class Store {
 
     private List<Product> productList = new ArrayList<>();
 
-    public void addProduct(Product product) {
-        this.productList.add(product);
-    }
-
-    /*public void addProductNotInListAndNotEpired(Product product) {
-        for (Product productItem : productList){
-            if (!(productItem.getName().equals(product)) && (productItem.getExpirationDay()!=product.getExpirationDay())){
-                this.productList.add(product);
-            }
-        }
-    }*/
-
     public List<Product> getProductList() {
         return productList;
     }
 
-    public int getNumberOfExpired() {
+    public boolean containByName(Product product) {
+        for (Product productItem : productList) {
+            if (productItem.getName().equals(product.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-        LocalDate now = LocalDate.now();
+    public boolean isValid(Product product) {
+        if (containByName(product) || product.isExpired()) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean addProduct(Product product) {
+        if (!isValid(product)) {
+            return false;
+        }
+        productList.add(product);
+        return true;
+    }
+
+    public int getNumberOfExpired() {
         int sum = 0;
         for (Product product : productList) {
-            if (product.getExpirationDay().isBefore(now)) {
+            if (product.isExpired()) {
                 sum += 1;
             }
         }
@@ -45,18 +54,20 @@ public class Store {
     public static void main(String[] args) {
 
         Store store = new Store();
+        store.productList.add(new Product("Milk", 2020, 11, 25));
+        store.productList.add(new Product("Tea", 2020, 11, 11));
 
-        store.addProduct(new Product("Milk", 2020, 11, 25));
+        store.addProduct(new Product("Milk", 2020, 11, 29));
         store.addProduct(new Product("Bread", 2020, 11, 27));
         store.addProduct(new Product("Butter", 2020, 11, 26));
-
-        /*store.addProductNotInListAndNotEpired(new Product("Milk",2020,11,29));
-        store.addProductNotInListAndNotEpired(new Product("Roll",2020,11,25));*/
+        store.addProduct(new Product("Butter",2020,11,28));
+        store.addProduct(new Product("Tea",2020,12,20));
+        store.addProduct(new Product("Sugar",2020,10,20));
 
         for (Product product : store.productList) {
             System.out.println(product.getName() + " : " + product.getExpirationDay());
         }
-
         System.out.println("The number of expired product: " + store.getNumberOfExpired());
     }
+
 }

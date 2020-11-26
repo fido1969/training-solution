bf71c0ab55aeaf74be949901eab50888a82ee027
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2020-11-26 21:42:31 +0100

    Product class was added

--- src/main/java/week05d04/Product.java
+++ src/main/java/week05d04/Product.java
@@ -10,8 +10,10 @@ public class Product {
     //A lejárati dátumot a konstruktorban állítsd össze.
     //Legyen mindkettőhöz getter metódus.
     //Készíts egy week05d04.Store osztályt, melyben van egy Productokat tároló lista. Legyen benne egy addProduct(Product) metódus.
-    //Bónusz: Legyen benne egy getNumberOfExpired() metódus ami visszaadja a lejárt termékek számát. Használd a LocalDate.now() és az isBefore() metódusokat.
-    //Tesztelj! (edited)
+    //Bónusz1: Legyen benne egy getNumberOfExpired() metódus ami visszaadja a lejárt termékek számát. Használd a LocalDate.now() és az isBefore() metódusokat.
+    //Bónusz2: Csak akkor engedj hozzáadni terméket, ha nem járt le és a neve még nem szerepel a listában.
+    //Ha sikerül hozzáadni akkor true-val térjen vissza, ha nem akkor false.
+    // Tesztelj! (edited)
 
     private String name;
     private LocalDate expirationDay;
@@ -27,5 +29,12 @@ public class Product {
 
     public LocalDate getExpirationDay() {
         return expirationDay;
+    }
+
+    public boolean isExpired() {
+        if (expirationDay.isBefore(LocalDate.now())) {
+            return true;
+        }
+        return false;
     }
 }

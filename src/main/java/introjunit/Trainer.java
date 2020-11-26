eb497f357e6c9e5d65294a273d4579e9ea93ba77
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2020-11-26 15:27:56 +0100

    Trainer class was added again

---
+++ src/main/java/introjunit/Trainer.java
@@ -0,0 +1,23 @@
+package introjunit;
+
+public class Trainer {
+
+    private String name;
+
+    public Trainer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNameUppercase() {
+        return name.toUpperCase();
+    }
+
+}

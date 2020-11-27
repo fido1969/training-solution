5d79d0b1562ec9c76d0af774a7ca00f5b18fe096
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2020-11-27 18:44:11 +0100

    Registration class was added

--- src/main/java/stringtype/registration/Registration.java
+++ src/main/java/stringtype/registration/Registration.java
@@ -21,6 +21,6 @@ public class Registration {
         System.out.println(userValidator.isValidPassword(password1, password2) ? "Correct!" : "Not correct!");
 
         System.out.print("Please give your email!");
-        System.out.println(userValidator.isValidEmail(scanner.nextLine()) ? "Correct!" : "Not correct!");
+        System.out.println(userValidator.isValidEmail("scanner.nextLine()") ? "Correct!" : "Not correct!");
     }
 }

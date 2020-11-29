c67dcfab732e8b44a9fd888738d20e2e64be293f
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2020-11-29 19:23:17 +0100

    Filters class was added

---
+++ src/main/java/algorithmsfilter/Filters.java
@@ -0,0 +1,21 @@
+package algorithmsfilter;
+
+import introconstructors.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Filters {
+
+   public List<Trainer> filterByName(List<Trainer> trainers, String part){
+
+       List<Trainer> filteredTrainers = new ArrayList<>();
+
+       for (Trainer trainer:trainers){
+           if (trainer.getName().contains(part)){
+               filteredTrainers.add(trainer);
+           }
+       }
+       return filteredTrainers;
+   }
+}

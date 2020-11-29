ae4a4873195e381bd81202327170ead2a3da58b1
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2020-11-29 14:16:22 +0100

    BankAccount class was added

---
+++ src/main/java/algorithmscount/bankaccounts/BankAccount.java
@@ -0,0 +1,26 @@
+package algorithmscount.bankaccounts;
+
+public class BankAccount {
+
+    private String name;
+    private String accountNumber;
+    private int balance;
+
+    public BankAccount(String name, String accountNumber, int balance) {
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+}

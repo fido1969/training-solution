aa631c8c2814d8f5d5d99abc91236136eefb93ae
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2020-11-29 11:26:17 +0100

    Salesperson class was added

---
+++ src/main/java/algorithmssum/sales/Salesperson.java
@@ -0,0 +1,20 @@
+package algorithmssum.sales;
+
+public class Salesperson {
+
+    private String name;
+    private int income;
+
+    public Salesperson(String name, int income) {
+        this.name = name;
+        this.income = income;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getIncome() {
+        return income;
+    }
+}

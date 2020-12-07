e998ca367cd78fc5255eb489be54e7d88faa83b1
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2020-12-07 09:21:07 +0100

    FileNameManipulator class was added

---
+++ src/main/java/stringmethods/filename/FileNameManipulator.java
@@ -0,0 +1,89 @@
+package stringmethods.filename;
+
+import java.util.Random;
+
+public class FileNameManipulator {
+
+    private boolean isEmpty(String str) {
+        return str == null || "".equals(str.trim());
+    }
+
+    public char findLastCharacter(String str) {
+        if (isEmpty(str)) {
+            throw new IllegalArgumentException("Empty string!");
+        }
+        str = str.trim();
+        return str.charAt(str.length() - 1);
+    }
+
+    public String StringfindFileExtension(String fileName) {
+        if (isEmpty(fileName)) {
+            throw new IllegalArgumentException("Invalid file name!");
+        }
+        fileName = fileName.trim();
+        int index = fileName.indexOf('.');
+        if (index < 1 || index == fileName.length() - 1) {
+            throw new IllegalArgumentException("Invalid file name!");
+        }
+        return fileName.substring(index);
+    }
+
+    public boolean identifyFilesByExtension(String ext, String fileName) {
+        if (isEmpty(fileName) || isEmpty(ext)) {
+            throw new IllegalArgumentException("Invalid argument!");
+        }
+        fileName = fileName.trim();
+        ext = ext.trim();
+        int index = fileName.lastIndexOf('.');
+        if (index < 1 || index == fileName.length() - 1) {
+            throw new IllegalArgumentException("Invalid argument!");
+        }
+        return fileName.endsWith(ext);
+    }
+
+    public boolean compareFilesByName(String searchedFileName, String actualFileName) {
+        if (isEmpty(searchedFileName) || isEmpty(actualFileName)) {
+            throw new IllegalArgumentException("Invalid argument!");
+        }
+        return searchedFileName.trim().equalsIgnoreCase(actualFileName.trim());
+    }
+
+    public String changeExtensionToLowerCase(String fileName) {
+        if (isEmpty(fileName)) {
+            throw new IllegalArgumentException("Empty string!");
+        }
+        fileName = fileName.trim();
+        int index = fileName.lastIndexOf('.');
+        if (index < 1 || index == fileName.length() - 1) {
+            throw new IllegalArgumentException("Invalid argument!");
+        }
+
+        String namePart = fileName.substring(0, index);
+        String extension = fileName.substring(index).toLowerCase();
+        return namePart + extension;
+    }
+
+    public String replaceStringPart(String fileName, String present, String target) {
+        if (isEmpty(fileName)) {
+            throw new IllegalArgumentException("Empty string!");
+        }
+
+        if (fileName.contains(present)) {
+            return fileName.replace(present, target);
+        }
+        return fileName;
+    }
+
+    public static void main(String[] args) {
+        FileNameManipulator fileNameManipulator = new FileNameManipulator();
+
+        System.out.println(fileNameManipulator.findLastCharacter("sdertd"));
+        System.out.println(fileNameManipulator.StringfindFileExtension("filename.com"));
+        //fileNameManipulator.identifyFilesByExtension(".exe", "file");
+        System.out.println(fileNameManipulator.changeExtensionToLowerCase("filename.COM"));
+        System.out.println(fileNameManipulator.replaceStringPart("filename.COM","COM","exe"));
+
+    }
+
+
+}

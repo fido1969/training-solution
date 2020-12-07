4da29a15da10537bacfcd61a18fe0ad76cc08b6d
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2020-12-07 18:20:10 +0100

    MathAlgorithmsTest class was corrected

--- src/test/java/week07d01/MathAlgorithmsTest.java
+++ src/test/java/week07d01/MathAlgorithmsTest.java
@@ -6,32 +6,63 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MathAlgorithmsTest {
 
+    @Test
+    public void primCanNotBeSmallerThanZeroTest(){
+        Exception ex=assertThrows(IllegalArgumentException.class,()->MathAlgorithms.isPrime(-6));
+        assertEquals("Wrong parameter!",ex.getMessage());
+    }
 
     @Test
     public void primCanNotBeZeroTest(){
         Exception ex=assertThrows(IllegalArgumentException.class,()->MathAlgorithms.isPrime(0));
-        assertEquals("'Number 'X'' can not be smaller than '2'!",ex.getMessage());
+        assertEquals("Wrong parameter!",ex.getMessage());
     }
 
     @Test
-    public void primCanNotBeOneTest(){
-        Exception ex=assertThrows(IllegalArgumentException.class,()->MathAlgorithms.isPrime(1));
-        assertEquals("'Number 'X'' can not be smaller than '2'!",ex.getMessage());
+    public void primIsOneTest(){
+        assertEquals(false,MathAlgorithms.isPrime(1));
     }
 
     @Test
-    public void primCanBelargerThanOneTest(){
-        Exception ex=assertThrows(IllegalArgumentException.class,()->MathAlgorithms.isPrime(-6));
-        assertEquals("'Number 'X'' can not be smaller than '2'!",ex.getMessage());
+    public void primIsTwoTest(){
+        assertEquals(true,MathAlgorithms.isPrime(2));
     }
 
     @Test
     public void isPrimeTest(){
-        assertEquals(true,MathAlgorithms.isPrime(11));
+        assertEquals(true,MathAlgorithms.isPrime(3));
+        assertEquals(true,MathAlgorithms.isPrime(5));
+        assertEquals(true,MathAlgorithms.isPrime(7));
+        assertEquals(true,MathAlgorithms.isPrime(13));
+        assertEquals(true,MathAlgorithms.isPrime(17));
     }
 
     @Test
     public void isNotPrimeTest(){
+        assertEquals(false,MathAlgorithms.isPrime(4));
+        assertEquals(false,MathAlgorithms.isPrime(6));
         assertEquals(false,MathAlgorithms.isPrime(8));
+        assertEquals(false,MathAlgorithms.isPrime(10));
+        assertEquals(false,MathAlgorithms.isPrime(12));
+    }
+
+    @Test
+    public void isPrimeOtherWayTest(){
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> MathAlgorithms.isPrimeOtherWay(-5));
+        assertEquals("Wrong parameter!",ex.getMessage());
+    }
+
+    @Test
+    public void isPrimeOtherWayZeroTest(){
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> MathAlgorithms.isPrimeOtherWay(0));
+        assertEquals("Wrong parameter!",ex.getMessage());
+    }
+
+    @Test
+    public void isPrimeOtherWayTest0(){
+        assertFalse(MathAlgorithms.isPrimeOtherWay(1));
+        assertTrue(MathAlgorithms.isPrimeOtherWay(5));
+        assertFalse(MathAlgorithms.isPrimeOtherWay(8));
+        assertTrue(MathAlgorithms.isPrimeOtherWay(11));
     }
 }

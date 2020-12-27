b38718e585ed57509b62a907f5a722aa95c79b7f
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2020-12-27 18:15:20 +0100

    Robot class was added

---
+++ src/test/java/week09d02/FibCalculatorTest.java
@@ -0,0 +1,16 @@
+package week09d02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FibCalculatorTest {
+    @Test
+    void sumEvensTest(){
+        FibCalculator fibCalculator = new FibCalculator();
+        assertEquals(0, fibCalculator.sumEvens(1));
+        assertEquals(2, fibCalculator.sumEvens(8));
+        assertEquals(188, fibCalculator.sumEvens(500));
+    }
+
+}

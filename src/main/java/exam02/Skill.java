020b36c712039aa5fbb95247bbde01e547e4a8f9
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2021-01-06 20:57:34 +0100

    Skill class was added

---
+++ src/main/java/exam02/Skill.java
@@ -0,0 +1,24 @@
+//Egy önéletrajzot reprezentáló osztályt kell létrehozni, melyben van egy név, valamint skillek
+//(hogy mihez ért az illető). A skill egy névvel és egy számmal (szint) rendelkezik, utóbbi 1 - 5 között.
+//Az önéletrajz a Cv osztály pár metódussal, a skill a Skill osztály.
+//A skillek megadása a nevével, valamint a szintjével történik, pl. programming (5).
+//A findSkillLevelByName() a skill neve alapján visszaadja annak szintjét.
+package exam02;
+
+public class Skill {
+    private String name; //1 - 5 között
+    private int level;
+
+    public Skill(String name, int level) {
+        this.name = name;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}

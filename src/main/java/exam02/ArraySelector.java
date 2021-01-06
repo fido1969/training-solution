1796b5358e7af537e2653976145c51ecc7488ccd
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2021-01-06 22:44:38 +0100

    ArraySelector class was added again with StringBuilder

--- src/main/java/exam02/ArraySelector.java
+++ src/main/java/exam02/ArraySelector.java
@@ -1,11 +1,9 @@
 
-//Írj egy olyan metódust az ArraySelector osztályban, mely egy kapott tömbből minden második elemet kiválogat, és egy szövegként összerak, szögletes zárójelek között!
-//A páros megállapítása index szerint működik. Azaz az első, nulla indexű elem páros, a második, azaz egyes indexű elem páratlan, stb.
-//Ha nulla elemű a tömb, akkor üres stringet adjon vissza!
 package exam02;
 
 public class ArraySelector {
-    public String selectEvens(int[] integerArray) {
+
+    public String selectEvens0(int[] integerArray) {
         if (integerArray.length == 0) {
             return "";
         }
@@ -18,7 +16,20 @@ public class ArraySelector {
         return selectedItems.substring(0, selectedItems.length() - 2) + "]";
     }
 
-    public static void main(String[] args) {
-        System.out.println(new ArraySelector().selectEvens(new int[]{1, 2, 3, 4, 5}));
+
+
+    public String selectEvens(int[] integerArray) {
+        if (integerArray.length == 0) {
+            return "";
+        }
+        StringBuilder stringBuilder = new StringBuilder("[");
+        stringBuilder.append(integerArray[0]);
+
+        for (int i = 1; i < integerArray.length; i++) {
+            if (i % 2 == 0) {
+                stringBuilder.append(", ").append(integerArray[i]);
+            }
+        }
+        return stringBuilder.append("]").toString();
     }
 }

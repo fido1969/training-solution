be553e131e2bd1a1bef7d1c94aaa93d412079e74
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2021-01-09 22:12:00 +0100

    Cv class was added again

--- src/main/java/exam02/Cv.java
+++ src/main/java/exam02/Cv.java
@@ -38,8 +38,10 @@ public class Cv {
 
     public void addSkills(String... skillsOfCv) {
         for (String item : skillsOfCv) {
-
-
+            int length = item.length();
+            String skillName = item.substring(0, length - 4);
+            Integer skillLevel = Integer.parseInt(item.substring(length - 2, length - 1));
+            this.skillList.add(new Skill(skillName, skillLevel));
         }
     }
 

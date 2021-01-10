9cfce51ec844b6a708c7e520e67da64538b8d4d2
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2021-01-10 20:20:11 +0100

    NumberGuesser class was added again

--- src/main/java/week04/NumberGuesser.java
+++ src/main/java/week04/NumberGuesser.java
@@ -1,3 +1,6 @@
+//Írj egy számkitaláló programot a GuessTheNumber osztályba! A program kitalál egy véletlenszerű számot
+// 1 és100 között. Majd bekér a felhasználótól ciklusban számokat. Mindig megmondja, hogy a szám,
+// kisebb, nagyobb vagy egyenlő-e mint a gondolt szám.!
 package week04;
 
 import java.util.Random;
@@ -6,15 +9,49 @@ import java.util.Scanner;
 public class NumberGuesser {
 
     public static void main(String[] args) {
-        int number = new Random().nextInt(100)+1;
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = new Random(12).nextInt(100) + 1;
+        System.out.println(number);
+
+        System.out.println("Guess the number between 1 & 100!");
+        System.out.println("---------------------------------");
+        int tip = 0;
+        int index=1;
+        while (tip != number && index<7) {
+            System.out.print("Give your tip: ");
+            try {
+                tip = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException nfr) {
+                System.out.println("You gave not a number");
+            }
+            if (tip > number) {
+                System.out.println("Your number larger!");
+            }
+            if (tip < number) {
+                System.out.println("Your number smaller!");
+            }
+            if (tip == number) {
+                System.out.println("Your tip is correct!");
+            }
+            index++;
+        }
+        if (tip!=number){
+            System.out.println("you lost the game!");
+        }
+    }
+
+   /* public static void main(String[] args) {
+        int number = new Random(1).nextInt(100)+1;
         Scanner scanner = new Scanner(System.in);
         System.out.println(number);
 
         boolean ok = false;
-
+        int guessed=0;
         while (!ok){
             System.out.print("Kérek egy számot [1..100] között: ");
-            int guessed=Integer.parseInt(scanner.nextLine());
+            guessed=Integer.parseInt(scanner.nextLine());
             if (guessed>number){
                 System.out.println("A gondolt szám kisebb!");
             }else if(guessed<number){
@@ -25,5 +62,5 @@ public class NumberGuesser {
             }
         }
 
-    }
+    }*/
 }

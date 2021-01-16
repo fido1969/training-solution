f2eb1456867c43102894e17a11944212c941cf85
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2021-01-16 20:22:08 +0100

    Pub class was added

---
+++ src/main/java/methoverloading/pub/Pub.java
@@ -0,0 +1,27 @@
+package methoverloading.pub;
+
+import methoverloading.Time;
+
+public class Pub {
+
+    private String name;
+    private Time openingTime;
+
+    public Pub(String name, int hours, int minutes) {
+        this.name = name;
+        this.openingTime = new Time(hours, minutes);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Time getOpeningTime() {
+        return openingTime;
+    }
+
+    @Override
+    public String toString() {
+        return name + " ; " + openingTime.getHours() + ":" + openingTime.getMinutes();
+    }
+}

7c98011964538d5e40ad22109219d4881b3b91c5
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2021-01-20 17:46:18 +0100

    testNumberStat class was added

---
+++ src/test/java/week12d3/NumberStatTest.java
@@ -0,0 +1,28 @@
+package week12d3;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberStatTest {
+
+    @Test
+    void testNumberStat() {
+        NumberStat ns = new NumberStat();
+        assertEquals(3, ns.numberFinder(List.of(1, 1, 5, 3, 4, 5, 6, 5, 6, 4, 1, 6, 5, 4)));
+        assertEquals(1, ns.numberFinder(List.of(1)));
+    }
+
+    @Test
+    void testNumberStatError() {
+        NumberStat ns = new NumberStat();
+        assertThrows(IllegalArgumentException.class, () -> ns.numberFinder(List.of()));
+        assertThrows(IllegalArgumentException.class, () -> ns.numberFinder(null));
+    }
+
+
+}

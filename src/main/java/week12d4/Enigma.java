bf9c102c8a21c54e9aeac3cda7e8a9fd9eb95afd
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2021-01-22 21:10:12 +0100

    Enigma class was added again

--- src/main/java/week12d4/Enigma.java
+++ src/main/java/week12d4/Enigma.java
@@ -1,3 +1,8 @@
+//Junior/Mid feladat: Töltsd le a példa bináris állomány a
+//https://github.com/Training360/strukturavalto-java-public/raw/master/examples/week12d04/src/main/resources/secret.dat címről!
+//Ez bájtokat tartalmaz, egy szöveg van titkosítva benne. Olvasd be bájtonként, és mindegyik bájthoz adjál hozzá
+//tízet! Ez valójában egy karakterkód, ezt alakítsd át `char` típusú értékké!
+//Majd egymás után írd ki ezeket a konzolra! Ne használj közöttük sortörést!
 package week12d4;
 
 import java.io.*;

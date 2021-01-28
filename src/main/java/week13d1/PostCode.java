80a1748ec4c06a1a08247abca55f1fe0b9cf4e9f
szabadkai.janos@freemail.hu <szabakai.janos@freemail.hu>
2021-01-28 16:15:59 +0100

    PostCode class was added

---
+++ src/main/java/week13d1/PostCode.java
@@ -0,0 +1,49 @@
+package week13d1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class PostCode {
+
+    //    A GitHubon a `examples\week13d01\src\main\resources\iranyitoszamok-varosok-2021.csv` fájl tartalmazza
+//    a magyar városok listáját.
+//    Add vissza a leghosszabb nevű várost!
+
+    public String readFromFile() {
+        Path file = Path.of("src/main/java/week13d1/irányitoszam.txt");
+        int maxLenght = 0;
+        int lineNumber = 0;
+        String longestCityName = "Unknown error!";
+        try (BufferedReader bf = new BufferedReader(Files.newBufferedReader(file))) {
+            String line;
+            headerSkipper(bf); //olvashatosag kedveert
+            String[] lineSplit;
+            while ((line = bf.readLine()) != null) {
+                lineNumber++;
+                String city = splitLine(line, lineNumber);
+                if (maxLenght < city.length()) {
+                    longestCityName = city;
+                    maxLenght = city.length();
+                }
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            throw new IllegalStateException("File not found");
+        }
+        return longestCityName;
+    }
+
+    private void headerSkipper(BufferedReader br) throws IOException {
+        br.readLine();
+    }
+
+    private String splitLine(String line, int lineNumber) throws IOException {
+        String[] temp = line.split(";");
+        if (temp.length < 1) {
+            throw new IOException("Wrong line" + lineNumber);
+        }
+        return temp[1].trim();
+    }
+}
